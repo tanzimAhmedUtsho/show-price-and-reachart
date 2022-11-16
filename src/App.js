ed2939437dc90852b36fb63137509ee5cbad6657
Tanzim Ahmed <utshotanzim@gmail.com>
2022-11-16 22:19:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./component/NavBar/NavBar";
+import Pricing from "./component/Pricing/Pricing";
+import AssignmentMarks from "./component/AssignmentMarks/AssignmentMarks";
+import PhoneBar from "./component/PhoneBar/PhoneBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar></NavBar>
+      <h1 className="text-4xl font-bold underline">This is a big big header</h1>
+      <p>This is just a paragraph</p>
+      <Pricing></Pricing>
+      <AssignmentMarks></AssignmentMarks>
+      <PhoneBar></PhoneBar>
     </div>
   );
 }
